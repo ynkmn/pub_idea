f57e6306e708d0e9c826766f4ef51cf2a0252b1b
ynkmn <y.nakamine@gmail.com>
2025-07-04 11:40:32 +0900

    250704.py を更新

--- 250704.py
+++ 250704.py
@@ -1,3 +1,13 @@
+with open('file.txt', 'r', encoding='utf-8') as file:
+    content = file.read()
+
+new_content = content.replace('置換前の文字列', '置換後の文字列')
+
+with open('file.txt', 'w', encoding='utf-8') as file:
+    file.write(new_content)
+
+
+
 lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 chunk_size = 4
 result = [lst[i:i+chunk_size] for i in range(0, len(lst), chunk_size)]
