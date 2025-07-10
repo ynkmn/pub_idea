524eb2174c07e7171323dc624e0c048bb2c7aadf
ynkmn <y.nakamine@gmail.com>
2025-07-10 14:16:50 +0900

    250710.py を更新

--- 250710.py
+++ 250710.py
@@ -1,5 +1,15 @@
+
+
+
 import numpy as np
 
 z_widths = np.array([0.2, 0.3, 0.5])
 z_heights = np.concatenate(([0], np.cumsum(z_widths)[:-1]))
 print(z_heights)  # [0.  0.2 0.5]
+
+
+df_filtered = df[
+    (df['列A'] == 条件1) &
+    (df['列B'] > 条件2) &
+    (df['列C'] < 条件3)
+]
